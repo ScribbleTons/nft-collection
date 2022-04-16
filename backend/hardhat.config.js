ea90e1a14a2907669574c1afa08f3b1fbecf6fc6
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2022-04-16 20:38:35 +0100

    prep config

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,16 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+//Uncomment when you add your API key
+// const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+// const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
-  networks: {
-    rinkeby: {
-      url: ALCHEMY_API_KEY_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
-    },
-  },
+  // networks: {
+  // rinkeby: {
+  //   url: ALCHEMY_API_KEY_URL,
+  //   accounts: [RINKEBY_PRIVATE_KEY],
+  // },
+  // },
 };
